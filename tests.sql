680c943c0285df87c827ad9b1f7aa6842f787712
Phạm Thanh Đạt <81749039+ptdat11@users.noreply.github.com>
2023-12-31 09:55:10 +0700

    Add files via upload

--- tests.sql
+++ tests.sql
@@ -2,7 +2,7 @@
 DECLARE @TongTien DECIMAL(19, 4)
 --
 EXEC @TongTien = NhapSach
-	@IDSach='3, 11, 17', 
+	@IDSach='6, 12, 18', 
 	@SoLuong='150, 160, 200', 
 	@Gia='100000, -1, 200000'
 --
@@ -16,15 +16,17 @@ DECLARE @ThanhTien DECIMAL(19, 4)
 EXEC @ThanhTien = BanSach
 	@IDKhachHang=1, 
 	@IDNvThanhToan=3, 
-	@IDSach='3, 11, 17', 
+	@IDSach='6, 12, 18', 
 	@SoLuong='1, 2, 1', 
-	@SoTienTra='200000'
+	@SoTienTra=200000
 --
 PRINT(@ThanhTien)
 SELECT * FROM Sach_LichSuNhapBan slsnb WHERE slsnb.IDLichSu = IDENT_CURRENT('LichSuNhapBanSach')
 SELECT * FROM HoaDon hd WHERE hd.ID = IDENT_CURRENT('HoaDon')
 SELECT * FROM LichSuTinhNo lstn WHERE lstn.ID = IDENT_CURRENT('LichSuTinhNo')
 
+DELETE Sach
+WHERE ID = 100
 
 ------ LIET KE SACH ------
 DECLARE @Keyword NVARCHAR(4000) = NULL,
@@ -51,4 +53,9 @@ DECLARE @From DATETIME = '1753-01-01 00:00:00.000',
 		@To DATETIME = '9999-12-31 23:59:59.997'
 --
 SELECT * FROM dbo.BaoCaoNo(@From, @To)
+
+
+SELECT *
+FROM sysusers
+WHERE issqluser = 1
 --
